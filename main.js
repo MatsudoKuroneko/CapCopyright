2bddb131a9c874377fe95a1f4b33787d32e0c9a8
MatsudoKuroneko <matsudo.kuroneko@icloud.com>
2021-03-11 19:03:17 +0900

    チャンネル取得部分だいたい完成

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const request = require("request");
+const canvas = require("canvas");
+const sec = process.argv[2];
+const thi = process.argv[3];
 
+function help(){
+    console.log("Usage: npm start commands");
+    console.log("Most used commands");
+    console.log("   create - [RECCOMEND] Create Capture with freetext and program title.");
+    console.log("   onlyid - Create Capture with program title");
+    console.log("   onlytext - Create Capture with freetext");
+    console.log("   listId - list all Channel IDs");
+    console.log("   listChannel - list all Channel IDs");
+}
 
 function getChannel() {
     request.get({
@@ -10,7 +22,6 @@ function getChannel() {
         if (err) {
             throw err;
         } else {
-            console.log(body.length);
             for (var i = 0; i < body.length; i++) {
                 console.log(body[i].channel + " " + body[i].name);
             }
@@ -20,7 +31,7 @@ function getChannel() {
     }))
 };
 
-function getID() {
+function getId() {
     request.get({
         url: "http://192.168.0.98:8888/api/channels",
         headers: { "Content-type": "application/json" },
@@ -29,7 +40,6 @@ function getID() {
         if (err) {
             throw err;
         } else {
-            console.log(body.length);
             for (var i = 0; i < body.length; i++) {
                 console.log(body[i].id + " " + body[i].name);
             }
@@ -47,16 +57,25 @@ function getProgramName() {
         if (err) {
             throw err;
         } else {
-            console.log(body.length);
-            for (var i = 0; i < body.length; i++) {
-                console.log(body[i].channel.id);
-            }
+            console.log(body.find(element => element.channel.id == thi).programs[0].name);
             //console.log(body[0].programs);
         }
 
     }))
 };
 
-getProgramName();
+
+if (sec == "help") {
+    help();
+} else if (sec == "listId") {
+    getId();
+} else if (sec == "listChannel") {
+    getChannel();
+} else if (sec == "create") {
+    getProgramName();
+}else{
+    help();
+}
+
 
 //出典: ラブライブ!虹ヶ咲学園スクールアイドル同好会 第10話「夏、はじまる
