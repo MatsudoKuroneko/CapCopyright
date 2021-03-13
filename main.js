6d4cb731248fd3e9a6bcff8a8fe2129d98c66a4d
MatsudoKuroneko <matsudo.kuroneko@gmail.com>
2021-03-13 15:22:16 +0900

    Canvas実装

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const request = require("request");
-const canvas = require("canvas");
+const chokidar = require("chokidar"); // フォルダ監視
+const { registerFont } = require("canvas");
+const { createCanvas, loadImage }= require("canvas");
+const fs = require('fs');
 const sec = process.argv[2];
-const thi = process.argv[3];
+const copy = process.argv[3];
+const ch = process.argv[4];
+var pname = "";
+
+registerFont('Makinas-4-Square.otf', { family: 'Makinas 4 Square' })
+
+const canvas = createCanvas(1440,810);
+const ctx = canvas.getContext("2d");
+
 
 function help(){
     console.log("Usage: npm start commands");
@@ -49,6 +60,51 @@ function getId() {
     }))
 };
 
+function stamp(f){
+    loadImage(f).then((image) => {
+        ctx.drawImage(image, 0, 0);
+        ctx.font = "bold 36px 'Makinas 4 Square'";
+        ctx.textAlign = "end";
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.strokeText("出典:  " + copy + " /",1430 + 2,794 - 48 + 2);
+        ctx.strokeText(pname,1430 + 2,794 + 2);
+        ctx.fillText("出典:  " + copy + " /",1430 + 2,794 - 48 + 2);
+        ctx.fillText(pname,1430 + 2,794 + 2);
+        ctx.strokeText("出典:  " + copy + " /",1430,794 - 48);
+        ctx.strokeText(pname,1430,794);
+        ctx.fillText("出典:  " + copy + " /",1430,794 - 48);
+        ctx.fillText(pname,1430,794);
+
+
+        const rf = f.replace("InComplete_","")
+        console.log(rf);
+        fs.unlinkSync(f);
+        const out = fs.createWriteStream(rf);
+        const stream = canvas.createJPEGStream();
+        stream.pipe(out)
+        out.on("finish",() => console.log("完了"));
+      })
+}
+
+function watch(){
+    watcher = chokidar.watch(__dirname,{
+        persistent:true,
+        ignoreInitial:true,
+        depth:0
+    });
+    watcher.on("add",f => {console.log(`file ${f} has been added`);
+    if(f.indexOf("InComplete"))
+    setTimeout(() => {
+        stamp(f);
+    }, 500);
+
+            })
+            .on("change",f => console.log(`file ${f} has been changed`))
+            .on("unlink",f => console.log(`file ${f} has been removed`));
+}
+
 function getProgramName() {
     request.get({
         url: "http://192.168.0.98:8888/api/schedule/broadcasting?time=0",
@@ -57,8 +113,10 @@ function getProgramName() {
         if (err) {
             throw err;
         } else {
-            console.log(body.find(element => element.channel.id == thi).programs[0].name);
+            pname = body.find(element => element.channel.id == ch).programs[0].name;
+            console.log(pname);
             //console.log(body[0].programs);
+            watch();
         }
 
     }))
@@ -78,4 +136,5 @@ if (sec == "help") {
 }
 
 
-//出典: ラブライブ!虹ヶ咲学園スクールアイドル同好会 第10話「夏、はじまる
+//出典:  プロジェクトラブライブ!虹ヶ咲学園スクールアイドル同好会 / ラブライブ!虹ヶ咲学園スクールアイドル同好会 第10話「夏、はじまる」
+//出典:  プロジェクトラブライブ!虹ヶ咲学園スクールアイドル同好会 / ラブライブ!虹ヶ咲学園スクールアイドル同好会 第2話「Cutest♡ガール」
